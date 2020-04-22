92c4dc42a43f6d2e9dbb2fb89fb22b7897cf5a7c
Adiomojeed <adio.mojeed@gmail.com>
2020-04-22 23:56:35 +0100

    Client side caching fixed

--- src/components/HackerNews.jsx
+++ src/components/HackerNews.jsx
@@ -10,16 +10,20 @@ const PATH = "https://hn.algolia.com/api/v1/search?query=";
 const PARAM_PAGE = "page=";
 
 class HackerNews extends Component {
+
+	_isMounted = false;
+	
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			result: null,
+			results: null,
 			searchTerm: DEFAULT_QUERY,
 			searchKey: "",
 			page: 0,
 		};
 
+		
 		this.setStory = this.setStory.bind(this);
 		this.fetchTopstories = this.fetchTopstories.bind(this);
 		this.HandleSearch = this.HandleSearch.bind(this);
@@ -30,24 +34,33 @@ class HackerNews extends Component {
 	}
 
 	setStory(result) {
-		this.setState({ result: {
-			[this.state.searchKey]: {result}
-		} });
+		const { searchKey, results } = this.state;
+		const {hits, page} = result
+		this.setState({
+			results: {
+				...results,
+				[searchKey]: { hits, page },
+			},
+		});
 	}
 	fetchTopstories(searchTerm, page) {
 		axios(`${PATH}${searchTerm}&${PARAM_PAGE}${page}`)
-			.then((result) => this.setStory(result.data.hits))
+			.then((result) => {
+				if (this._isMounted) {
+					this.setStory(result.data)
+				}
+			})
 			.catch((error) => error);
 	}
 	componentDidMount() {
+		this._isMounted = true
 		const { searchTerm, page, result, searchKey } = this.state;
 		this.setState({ searchKey: searchTerm });
 		this.fetchTopstories(searchTerm, page);
-		console.log(result);
 	}
 
-	componentDidUpdate() {
-		console.log(this.state.result);
+	componentWillUnmount() {
+		this._isMounted = false;
 	}
 
 	handleNext() {
@@ -69,8 +82,8 @@ class HackerNews extends Component {
 	}
 
 	HandleSearch(e) {
-		const { searchTerm } = this.state;
-		this.setState({ result: null, searchKey: searchTerm });
+		const { searchTerm, results } = this.state;
+		this.setState({ searchKey: searchTerm, results: {...results} });
 		this.fetchTopstories(searchTerm, 0);
 		e.preventDefault();
 	}
@@ -82,7 +95,12 @@ class HackerNews extends Component {
 	}
 
 	render() {
-		const { result, searchTerm, error, searchKey } = this.state;
+		const { results, searchTerm, error, searchKey } = this.state;
+		const actualResult = (
+			results &&
+			results[searchKey] &&
+			results[searchKey]['hits']
+		  ) || [];
 		const override = css`
 			display: block;
 			margin: 20px auto;
@@ -99,14 +117,14 @@ class HackerNews extends Component {
 					onHandleChange={this.HandleChange}
 					onHandleSearch={this.HandleSearch}
 				/>
-				{!result ? (
+				{!results ? (
 					<BounceLoader css={override} size={80} color={"#55efc4"} />
 				) : (
 					<React.Fragment>
 						<h2>Search Results...</h2>
 						<table>
 							<Table
-								result={result}
+								result={actualResult}
 								searchTerm={searchTerm}
 								onDismiss={this.onDismiss}
 							/>
